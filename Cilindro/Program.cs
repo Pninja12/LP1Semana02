15a4824957fbf6b4f75e771aa83158bc6b247e3d
Paulo Silva <pauloalmeida1288@gmail.com>
2024-02-28 19:16:30 +0000

    Começo do Ex5, criação das variáveis

---
+++ Cilindro/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual a altura do cilindro: ");
+            string resposta = Console.ReadLine(); //recebe variável
+            float altura = float.Parse(resposta); //transforma variável 
+                                                  //string em float
+            Console.Write("Qual o raio do cilindro: ");
+            resposta = Console.ReadLine(); //recebe variável
+            float raio = float.Parse(resposta); //transforma variável 
+                                                //string em float
+        }
+    }
+}
