507e4ab27fbe22e3404814c7a3713d3abec9a65f
Paulo Silva <pauloalmeida1288@gmail.com>
2024-02-28 18:47:39 +0000

    Ex1 concluído e testado

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insere um número inteiro não-negativo: ");
+            string resposta = Console.ReadLine();
+            byte n = byte.Parse(resposta);
+
+            Console.WriteLine($"{--n}\n{++n}");
+        }
+    }
+}
