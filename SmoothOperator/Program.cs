32c26ef4f31a15767e2c0a8c104f6d9154edffa7
Paulo Silva <pauloalmeida1288@gmail.com>
2024-02-28 18:49:14 +0000

    Ex1 com comentários

--- SmoothOperator/Program.cs
+++ SmoothOperator/Program.cs
@@ -4,11 +4,16 @@ namespace SmoothOperator
 {
     class Program
     {
+        /// <summary>
+        /// Programa que pede número e mostra esse número
+        /// incrementado e decrementado
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.Write("Insere um número inteiro não-negativo: ");
-            string resposta = Console.ReadLine();
-            byte n = byte.Parse(resposta);
+            string resposta = Console.ReadLine(); //recebe variável
+            byte n = byte.Parse(resposta); //transforma variável string em byte
 
             Console.WriteLine($"{--n}\n{++n}");
         }
