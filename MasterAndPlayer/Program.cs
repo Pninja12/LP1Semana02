d2e3a4a748cd0269fd4131790fdba23a5beee442
Paulo Silva <pauloalmeida1288@gmail.com>
2024-03-01 14:15:44 +0000

    Ex Extra código acabado mas não testado

--- MasterAndPlayer/Program.cs
+++ MasterAndPlayer/Program.cs
@@ -6,7 +6,7 @@ namespace MasterAndPlayer
     {
         static void Main(string[] args)
         {
-            Console.Write("Number Master tell me a number between 0 and 100: ");
+            Console.Write("Number Master insert number: ");
             string number = Console.ReadLine();
             int n = int.Parse(number);
             if (n < 100 && n > 0){
@@ -14,13 +14,28 @@ namespace MasterAndPlayer
                     Console.WriteLine("");
                 }
                 while (true){
-                    Console.Write("Player choose a number between 0 and 100: ");
+                    Console.Write("Player insert number: ");
                     string number2 = Console.ReadLine();
-                    int i = int.Parse(number);
+                    int i = int.Parse(number2);
+                    if (i > 100 && i < 0)
+                    {
+                        Console.WriteLine("Invalid number! Try again!");
+                    }
+                    else if (i > n){
+                        Console.Write("The correct number is higher than ");
+                        Console.WriteLine($"{i}.");
+                    }
+                    else if (i < n){
+                        Console.Write("The correct number is lower than ");
+                        Console.WriteLine($"{i}.");
+                    }
+                    else{Console.Write("Player wins!");
+
+                    }
                 }
             }
             else{
-                Console.WriteLine("Number not acceptable");
+                Console.WriteLine("Invalid number!");
             }
 
         }
