1c58fe02b826f1e8b35bd900805dc9fee26107ba
Paulo Silva <pauloalmeida1288@gmail.com>
2024-03-01 14:22:23 +0000

    Ex Extra código corrigido.Número errado não funcionava, o jogador não ganhava, o maior e o menor estavam trocados e o loop das 100 linhas não emprimia. Tudo resolvido

--- MasterAndPlayer/Program.cs
+++ MasterAndPlayer/Program.cs
@@ -10,27 +10,28 @@ namespace MasterAndPlayer
             string number = Console.ReadLine();
             int n = int.Parse(number);
             if (n < 100 && n > 0){
-                for(int i = 0; i>100;i++){
-                    Console.WriteLine("");
+                for(int i = 0; i<100;i++){
+                    Console.WriteLine(" ");
                 }
                 while (true){
                     Console.Write("Player insert number: ");
                     string number2 = Console.ReadLine();
                     int i = int.Parse(number2);
-                    if (i > 100 && i < 0)
+                    if (i > 100 || i < 0)
                     {
                         Console.WriteLine("Invalid number! Try again!");
                     }
-                    else if (i > n){
+                    else if (i < n){
                         Console.Write("The correct number is higher than ");
                         Console.WriteLine($"{i}.");
                     }
-                    else if (i < n){
+                    else if (i > n){
                         Console.Write("The correct number is lower than ");
                         Console.WriteLine($"{i}.");
                     }
-                    else{Console.Write("Player wins!");
-
+                    else{
+                        Console.Write("Player wins!");
+                        break;
                     }
                 }
             }
