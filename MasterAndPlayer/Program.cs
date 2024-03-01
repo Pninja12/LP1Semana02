934a590766e7e9f58580b9f3ea3ce690f1316812
Paulo Silva <pauloalmeida1288@gmail.com>
2024-03-01 14:02:35 +0000

    Ex Extra feito até ao loop

---
+++ MasterAndPlayer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MasterAndPlayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Number Master tell me a number between 0 and 100: ");
+            string number = Console.ReadLine();
+            int n = int.Parse(number);
+            if (n < 100 && n > 0){
+                for(int i = 0; i>100;i++){
+                    Console.WriteLine("");
+                }
+                while (true){
+                    Console.Write("Player choose a number between 0 and 100: ");
+                    string number2 = Console.ReadLine();
+                    int i = int.Parse(number);
+                }
+            }
+            else{
+                Console.WriteLine("Number not acceptable");
+            }
+
+        }
+    }
+}
